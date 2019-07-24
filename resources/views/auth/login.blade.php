4b876c96089133a19fe2833ad222336b9abea685
tcliap <maria.aguirre@tigo.net.py>
2019-07-24 09:10:05 -0400

    login modificado

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,7 +7,7 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading "><span class=""> PORTAL DE NÓMINAS</span></div>
+                    <div class="panel-heading "><span class="text-blue text-bold"> PORTAL DE NÓMINAS</span></div>
 
                     <div class="panel-body">
                         <form class="form-horizontal" method="POST" action="{{ route('login') }}">

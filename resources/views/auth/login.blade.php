47a2948c4ac93f526fc68428c1dd1c12c3d1ba9c
tcliap <maria.aguirre@tigo.net.py>
2019-07-24 09:13:44 -0400

    vista login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,7 +7,7 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading "><span class="text-info text-boldgi"> PORTAL DE NÓMINAS</span></div>
+                    <div class="panel-heading "><span class="text-info text-bold"> PORTAL DE NÓMINAS</span></div>
 
                     <div class="panel-body">
                         <form class="form-horizontal" method="POST" action="{{ route('login') }}">

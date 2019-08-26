70319ce2ae6697bb3d6f5a2023ff0ed58a2ff4fb
tcliap <maria.aguirre@tigo.net.py>
2019-08-26 10:58:50 -0400

    no

--- resources/views/consideraciones_directa/search_aprobacion.blade.php
+++ resources/views/consideraciones_directa/search_aprobacion.blade.php
@@ -1,28 +1,18 @@
-<<<<<<< HEAD
-{!! Form::open(array('url'=>'nomina_directa','method'=>'GET','autocomplete'=>'off','role'=>'search')) !!}
-<div class="form-group form-inline">
-
-    <select name="id_persona" class="form-control selectpicker text-uppercase col-lg-4" id="id_representante" title="Seleccione Representante" data-live-search="true">
-        @foreach($personas as $persona)
-=======
 {!! Form::open(array('url'=>'aprobacion_consideraciones_directa/'.$mes,'method'=>'GET','autocomplete'=>'off','role'=>'search')) !!}
 <div class="form-group form-inline">
 
     <select name="id_persona" class="form-control selectpicker text-uppercase col-lg-4" id="id_representante" title="Seleccione Representante" data-live-search="true">
         @foreach($personas_consideracion as $persona)
->>>>>>> excel
             <option value="{{$persona->id_persona_directa}}">ch: {{$persona->personaDirecta->ch}} - {{$persona->personaDirecta->nombre}}</option>
         @endforeach
     </select>
 
-<<<<<<< HEAD
-=======
+
     <select name="id_consideracion" class="form-control selectpicker text-uppercase col-lg-4" id="id_consideracion" title="Seleccione consideracion" data-live-search="true">
         @foreach($consideraciones as $consideracion)
             <option value="{{$consideracion->id}}">{{$consideracion->nombre}}</option>
         @endforeach
     </select>
->>>>>>> excel
     <select name="id_jefe" class="form-control selectpicker text-uppercase col-lg-4" id="id_jefe" title="Seleccione Jefe" data-live-search="true">
         @foreach($jefes as $jefe)
             <option value="{{$jefe->id_persona}}">ch:{{$jefe->ch}} - {{$jefe->nombre}}</option>
@@ -30,7 +20,6 @@
     </select>
 
     <select name="id_zona" class="form-control selectpicker text-uppercase col-lg-4" id="id_zona" title="Seleccione Zona" data-live-search="true">
-<<<<<<< HEAD
         @foreach($zonas_user as $zona)
             <option value="{{$zona->id}}">{{$zona->id}} - {{$zona->zona}}</option>
         @endforeach
@@ -42,12 +31,10 @@
     </select>
 
     <input type="number" name="mes" class="form-control text-uppercase" placeholder="MES: YYYYMM">
-=======
         @foreach($zonas as $zona)
             <option value="{{$zona->id}}">{{$zona->representante_zonal_nombre}} - {{$zona->zona}}</option>
         @endforeach
     </select>
->>>>>>> excel
 
     <button type="submit" class="btn btn-primary "><span>Buscar </span><i class="fa fa-search col-lg-2" aria-hidden="true"></i></button>
 

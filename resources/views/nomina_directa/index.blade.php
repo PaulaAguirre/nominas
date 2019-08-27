f966aebb9abc3a90ac7f6642f6a5719f158ecd64
tcliap <maria.aguirre@tigo.net.py>
2019-08-27 12:23:02 -0400

    generar

--- resources/views/nomina_directa/index.blade.php
+++ resources/views/nomina_directa/index.blade.php
@@ -6,17 +6,13 @@
                 @if(auth()->user()->hasRoles(['tigo_people_admin']))
                     <a href="ingresar_nuevo_asesor"><button class="btn btn-facebook">Ingresos Mes Actual {{\Carbon\Carbon::now()->format('Y-m')}}</button></a>
                 @else
-                    @if(\Carbon\Carbon::today() < (new Carbon\Carbon('first day of this month'))->addDay(14))
+                    @if(\Carbon\Carbon::today() < (new Carbon\Carbon('first day of this month'))->addDay(29))
                         <a href="ingresar_nuevo_asesor"><button class="btn btn-facebook" data-toggle="tooltip" data-placement="top" title={{'hasta:'.$fecha_inicio}}>Ingresos Mes Actual {{\Carbon\Carbon::now()->format('Y-m')}}</button></a>
                     @endif
                 @endif
 
                 @if(auth()->user()->hasRoles(['tigo_people_admin']))
                     <a href="nomina_directa/create"><button class="btn btn-success">Generar Nomina  {{\Carbon\Carbon::now()->addMonths(1)->format('Y-m')}}</button></a>
-                @else
-                    @if($today->between($fecha_inicio, $fecha_fin))
-                        <a href="nomina_directa/create"><button class="btn btn-success" data-toggle="tooltip" data-placement="top" title={{'hasta:'.$fecha_fin}}>Generar Nomina  {{\Carbon\Carbon::now()->addMonths(1)->format('Y-m')}}</button></a>
-                    @endif
                 @endif
             </h3>
           <p class="text-info" id="cantidad">Cantidad</p>

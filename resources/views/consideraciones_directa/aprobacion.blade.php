8f21785e0db5b4a996d2b6fbb5da076b329c6c44
tcliap <maria.aguirre@tigo.net.py>
2019-10-24 08:39:21 -0300

    tamaño

--- resources/views/consideraciones_directa/aprobacion.blade.php
+++ resources/views/consideraciones_directa/aprobacion.blade.php
@@ -24,25 +24,25 @@
         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
             <div class="table-responsive">
                 <table class="table table-striped table-bordered table-condensed table-hover" id="tabla_persona">
-                    <thead class="text-center" style="background-color: #8eb4cb">
+                    <thead class="text-center text-sm" style="background-color: #8eb4cb" >
                     <th>id</th>
                     <th>CH</th>
                     <th>Representante</th>
-                    <th>Zona</th>
+                    <th class>Zona</th>
                     <th>Rep Zonal / jefe</th>
                     <th>Consideración</th>
                     <th>Detalles</th>
-                    <th>Regularización</th>
+                    <th>Regularizar</th>
                     <th>Aprobar</th>
-                    <th class="col-md-1">Porc%</th>
+                    <th >Porc%</th>
                     <th>Comentarios</th>
-                    <th>Rechazo</th>
-                    <th>Enviar</th>
+                    <th>Rechazado</th>
+                    <th>Opc</th>
 
                     </thead>
                     <tbody id="ajuste">
                     @foreach ($personas_consideracion as $persona)
-                        <tr class="text-uppercase">
+                        <tr class="text-uppercase text-sm">
                             @if (auth()->user()->hasRoles(['tigo_people']))
                                 @if(auth()->user()->zonas->contains($persona->personaDirecta->id_zona))
                                     <td>{{$persona->id_nomina}}</td>
@@ -54,7 +54,7 @@
                                     <td>{{$persona->detalles_consideracion}}</td>
                                     <td>{{$persona->regularizacion_consideracion}}</td>
                                     <td id="tdaprobacion">
-                                        <select name="aprobacion[]" class="form-control aprobacion" id="aprobacion-{{$persona->id_nomina}}">
+                                        <select name="aprobacion[]" class="form-control aprobacion text-sm" id="aprobacion-{{$persona->id_nomina}}">
                                             <option value="aprobado" >aprobado</option>
                                             <option value="rechazado">rechazado</option>
                                             <option value="pendiente" selected>pendiente</option>
@@ -79,14 +79,14 @@
                                 <td>{{$persona->detalles_consideracion}}</td>
                                 <td>{{$persona->regularizacion_consideracion}}</td>
                                 <td id="tdaprobacion">
-                                    <select name="aprobacion[]" class="form-control aprobacion selectpicker" id="aprobacion-{{$persona->id_nomina}}">
+                                    <select name="aprobacion[]" class="form-control aprobacion selectpicker text-sm" id="aprobacion-{{$persona->id_nomina}}">
                                         <option value="aprobado" >aprobado</option>
                                         <option value="rechazado">rechazado</option>
                                         <option value="pendiente" selected>pendiente</option>
                                     </select>
                                 </td>
                                 <td id="td_objetivo">
-                                    <select name="objetivo[]" class="form-control text-uppercase" style="display:none;"  id="objetivo-{{$persona->id_nomina}}">
+                                    <select name="objetivo[]" class="form-control text-uppercase text-sm" style="display:none;"  id="objetivo-{{$persona->id_nomina}}">
                                         <option value="100%">100%</option>
                                         <option value="75% nuevo">75% nuevo</option>
                                         <option value="75%">75%</option>
@@ -96,9 +96,9 @@
                                         <option value="sin objetivos">Sin objetivos</option>
                                         </select>
                                 </td>
-                                <td><textarea rows="3" class="form-control text-uppercase" style="display:none;" name="comentario_consideracion[]" id="comentario_cosideracion-{{$persona->id_nomina}}"></textarea><input type="hidden" ></td>
+                                <td><textarea rows="3" class="form-control text-uppercase text-sm" style="display:none;" name="comentario_consideracion[]" id="comentario_cosideracion-{{$persona->id_nomina}}"></textarea><input type="hidden" ></td>
 
-                                <td><textarea rows="3"  class="form-control text-uppercase" style="display:none;" name="motivo_rechazo[]" id="motivo_rechazo-{{$persona->id_nomina}}"></textarea><input type="hidden" ></td>
+                                <td><textarea rows="3"  class="form-control text-uppercase text-sm" style="display:none;" name="motivo_rechazo[]" id="motivo_rechazo-{{$persona->id_nomina}}"></textarea><input type="hidden" ></td>
                                 <td class="text-center">
                                     <input name="_token" value="{{csrf_token()}}" type="hidden">
 

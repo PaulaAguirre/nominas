f8863a6b2e1815670e02bad544f513625d4e1d7a
tcliap <maria.aguirre@tigo.net.py>
2019-11-21 11:12:19 -0300

    fechas

--- app/NominaDirecta.php
+++ app/NominaDirecta.php
@@ -85,7 +85,7 @@ class NominaDirecta extends Model
         else
         {
             $fecha1 = new Carbon('first day of this month');
-            $fecha2 = (new Carbon('first day of this month'))->addDays(27);
+            $fecha2 = (new Carbon('first day of this month'))->addDays(19);
             $mes = Carbon::now();
             if ($mes->between($fecha1, $fecha2))
             {
@@ -94,7 +94,7 @@ class NominaDirecta extends Model
             else
             {
                // $mes_format = $mes->addMonth(1)->format('Ym');
-                $mes_format = 201911;
+                $mes_format = 201912;
                 //dd($mes);
             }
 

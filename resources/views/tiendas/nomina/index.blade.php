39e85b865e0d977e1f08dfb0c42815ef378688b8
tcliap <maria.aguirre@tigo.net.py>
2020-01-08 15:05:44 -0300

    nominas

--- resources/views/tiendas/nomina/index.blade.php
+++ resources/views/tiendas/nomina/index.blade.php
@@ -45,51 +45,53 @@
                     <th class="text-center col-lg-1">Opciones</th>
                     </thead>
                     @foreach ($asesores as $asesor)
-                        <tr class="text-uppercase text-sm">
-                            <td>{{$asesor->id}}</td>
-                            <td>{{$asesor->mes}}</td>
-                            <td>{{$asesor->asesor ?  $asesor->asesor->ch : ''}}</td>
-                            <td>{{$asesor->asesor ? $asesor->asesor->nombre : ''}}</td>
-                            <td>{{$asesor->asesor ? $asesor->asesor->cargo_go : ''}}</td>
-                            <td>{{$asesor->asesor->especialista == 'si' ? 'si': 'no'}}</td>
-                            <td>{{$asesor->asesor->tienda->zona->zona.' / '.$asesor->asesor->tienda->zona->representante_zonal_nombre}}</td>
-                            <td>{{$asesor->asesor->tienda->tienda_nombre}} / {{$asesor->asesor->tienda->jefetienda ? $asesor->asesor->tienda->jefetienda->nombre : 'Sin Jefe'}}</td>
-                            <td>{{$asesor->asesor->teamleader ? $asesor->asesor->teamleader->nombre : ''}}</td>
-                            <td><span class="text-info">Cons.:</span> {{$asesor->consideracion ? $asesor->consideracion->nombre : ''}}<br><span class="text-danger">Estado: </span>{{$asesor->estado_consideracion}}</td>
-                            @if($asesor->estado_inactivacion == 'pendiente')
-                                <td class="text-warning">pendiente</td>
-                            @elseif($asesor->estado_inactivacion == 'aprobado')
-                                <td class="text-danger">Inactivo</td>
-                            @else
-                                <td class="text-success">Activo</td>
-                            @endif
-                            <td>{{$asesor->porcentaje_objetivo}}</td>
-                            <td>
-                                @if(auth()->user()->hasRoles(['zonal']))
-                                    @if((\Carbon\Carbon::today() < (new Carbon\Carbon('first day of this month'))->addDay(31)))
-                                    <a href="{{URL::action('AsesorTiendaController@edit', $asesor->asesor->id)}}">
-                                        <button class="btn btn-warning btn-xs" data-toggle="tooltip" data-placement="top" title="Editar Datos del Asesor"><i class="fa fa-pencil"></i></button>
-                                    </a>
-                                    @if(!$asesor->estado_consideracion)
-                                        <a href="" data-target="#modal-consideracion-store-{{$asesor->id}}" data-toggle="modal" data-placement="top" title="Consideración" ><button class="btn btn-xs btn-facebook"><i class="fa fa-comment" aria-hidden="true"></i></button></a>
-                                    @endif
-                                    @if(!$asesor->estado_inactivacion)
-                                        <a href="" data-target="#modal-nomina-delete-{{$asesor->id}}" data-toggle="modal" data-placement="top" title="inactivar" ><button class="btn btn-xs btn-danger"><i class="fa fa-user-times" aria-hidden="true"></i></button></a>
-                                    @endif
-                                    @endif
+                        @if($asesor->$asesor)
+                            <tr class="text-uppercase text-sm">
+                                <td>{{$asesor->id}}</td>
+                                <td>{{$asesor->mes}}</td>
+                                <td>{{$asesor->asesor ?  $asesor->asesor->ch : ''}}</td>
+                                <td>{{$asesor->asesor ? $asesor->asesor->nombre : ''}}</td>
+                                <td>{{$asesor->asesor ? $asesor->asesor->cargo_go : ''}}</td>
+                                <td>{{$asesor->asesor->especialista == 'si' ? 'si': 'no'}}</td>
+                                <td>{{$asesor->asesor->tienda->zona->zona.' / '.$asesor->asesor->tienda->zona->representante_zonal_nombre}}</td>
+                                <td>{{$asesor->asesor->tienda->tienda_nombre}} / {{$asesor->asesor->tienda->jefetienda ? $asesor->asesor->tienda->jefetienda->nombre : 'Sin Jefe'}}</td>
+                                <td>{{$asesor->asesor->teamleader ? $asesor->asesor->teamleader->nombre : ''}}</td>
+                                <td><span class="text-info">Cons.:</span> {{$asesor->consideracion ? $asesor->consideracion->nombre : ''}}<br><span class="text-danger">Estado: </span>{{$asesor->estado_consideracion}}</td>
+                                @if($asesor->estado_inactivacion == 'pendiente')
+                                    <td class="text-warning">pendiente</td>
+                                @elseif($asesor->estado_inactivacion == 'aprobado')
+                                    <td class="text-danger">Inactivo</td>
                                 @else
-                                    <a href="{{URL::action('AsesorTiendaController@edit', $asesor->asesor->id)}}">
-                                        <button class="btn btn-warning btn-xs" data-toggle="tooltip" data-placement="top" title="Editar Datos del Asesor"><i class="fa fa-pencil"></i></button>
-                                    </a>
-                                    @if(!$asesor->estado_consideracion)
-                                        <a href="" data-target="#modal-consideracion-store-{{$asesor->id}}" data-toggle="modal" data-placement="top" title="Consideración" ><button class="btn btn-xs btn-facebook"><i class="fa fa-comment" aria-hidden="true"></i></button></a>
-                                    @endif
-                                    @if(!$asesor->estado_inactivacion)
-                                        <a href="" data-target="#modal-nomina-delete-{{$asesor->id}}" data-toggle="modal" data-placement="top" title="inactivar" ><button class="btn btn-xs btn-danger"><i class="fa fa-user-times" aria-hidden="true"></i></button></a>
-                                    @endif
+                                    <td class="text-success">Activo</td>
                                 @endif
-                            </td>
-                        </tr>
+                                <td>{{$asesor->porcentaje_objetivo}}</td>
+                                <td>
+                                    @if(auth()->user()->hasRoles(['zonal']))
+                                        @if((\Carbon\Carbon::today() < (new Carbon\Carbon('first day of this month'))->addDay(31)))
+                                        <a href="{{URL::action('AsesorTiendaController@edit', $asesor->asesor->id)}}">
+                                            <button class="btn btn-warning btn-xs" data-toggle="tooltip" data-placement="top" title="Editar Datos del Asesor"><i class="fa fa-pencil"></i></button>
+                                        </a>
+                                        @if(!$asesor->estado_consideracion)
+                                            <a href="" data-target="#modal-consideracion-store-{{$asesor->id}}" data-toggle="modal" data-placement="top" title="Consideración" ><button class="btn btn-xs btn-facebook"><i class="fa fa-comment" aria-hidden="true"></i></button></a>
+                                        @endif
+                                        @if(!$asesor->estado_inactivacion)
+                                            <a href="" data-target="#modal-nomina-delete-{{$asesor->id}}" data-toggle="modal" data-placement="top" title="inactivar" ><button class="btn btn-xs btn-danger"><i class="fa fa-user-times" aria-hidden="true"></i></button></a>
+                                        @endif
+                                        @endif
+                                    @else
+                                        <a href="{{URL::action('AsesorTiendaController@edit', $asesor->asesor->id)}}">
+                                            <button class="btn btn-warning btn-xs" data-toggle="tooltip" data-placement="top" title="Editar Datos del Asesor"><i class="fa fa-pencil"></i></button>
+                                        </a>
+                                        @if(!$asesor->estado_consideracion)
+                                            <a href="" data-target="#modal-consideracion-store-{{$asesor->id}}" data-toggle="modal" data-placement="top" title="Consideración" ><button class="btn btn-xs btn-facebook"><i class="fa fa-comment" aria-hidden="true"></i></button></a>
+                                        @endif
+                                        @if(!$asesor->estado_inactivacion)
+                                            <a href="" data-target="#modal-nomina-delete-{{$asesor->id}}" data-toggle="modal" data-placement="top" title="inactivar" ><button class="btn btn-xs btn-danger"><i class="fa fa-user-times" aria-hidden="true"></i></button></a>
+                                        @endif
+                                    @endif
+                                </td>
+                            </tr>
+                        @endif
                             @include('tiendas.asesores.modal_eliminacion')
                             @include('tiendas.consideraciones.crear_consideracion_modal')
                     @endforeach

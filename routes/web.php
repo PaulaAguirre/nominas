55ee23275b826cd3bf50a2dcd20e9433773a37ba
tcliap <maria.aguirre@tigo.net.py>
2020-04-11 19:34:11 -0400

    mes add

--- routes/web.php
+++ routes/web.php
@@ -230,7 +230,7 @@ Route::get('/excel_directa_mes_anterior', 'ExcelController@exportarDirectaMesAnt
 
 Route::get('variable', function ()
 {
-    dd(Config::get('global.cadena'));
+    dd(Config::get('mes', 202004));
 });
 
 

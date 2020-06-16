dd938bbac114600040886d4987e9f4ae9c3507a7
tcliap <maria.aguirre@tigo.net.py>
2020-06-16 16:42:59 -0400

    tiendas hab

--- config/global.php
+++ config/global.php
@@ -6,8 +6,8 @@
 return [
     'mes' => 202006,
     'mes_anterior' => 202005,
-    'mes_tienda' => 202006,
-    'mes_anterior_tienda'=>202005,
+    'mes_tienda' => 202007,
+    'mes_anterior_tienda'=>202006,
     'mes_anterior_indirecta'=>202002,
     'mes_indirecta'=>202007,
 ];

8ddf6aa163083b99e9d02cd7e9888f3b553642f6
tcliap <maria.aguirre@tigo.net.py>
2020-06-18 11:31:47 -0400

    cambio de fecha directa

--- config/global.php
+++ config/global.php
@@ -4,11 +4,13 @@
  */
 
 return [
-    'mes' => 202006,
-    'mes_anterior' => 202005,
+    'mes' => 202007,
+    'mes_anterior' => 202006,
+
     'mes_tienda' => 202007,
     'mes_anterior_tienda'=>202006,
-    'mes_anterior_indirecta'=>202002,
+
+    'mes_anterior_indirecta'=>202006,
     'mes_indirecta'=>202007,
 ];
 

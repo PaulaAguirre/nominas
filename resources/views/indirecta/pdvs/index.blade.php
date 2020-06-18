ac28ca5e4029effdb8c6e48c82adc066c877bec1
tcliap <maria.aguirre@tigo.net.py>
2020-06-18 18:49:35 -0400

    cambios en index pdv

--- resources/views/indirecta/pdvs/index.blade.php
+++ resources/views/indirecta/pdvs/index.blade.php
@@ -36,9 +36,10 @@
                             <td>{{$pdv->codigo}}</td>
                             <td>{{$pdv->nombre}}</td>
                             <td>{{$pdv->circuito ? $pdv->circuito->codigo : ''}}</td>
+                            <td>{{$pdv->impulsador ? $pdv->impulsador->ch :''}}</td>
                             <td>{{$pdv->impulsador ? $pdv->impulsador->coordinador->nombre : ''}}</td>
                             <td>{{$pdv->impulsador ? $pdv->impulsador->nombre : ''}}</td>
-                            <td>{{$pdv->impulsador ? $pdv->impulsador->ch :''}}</td>
+
                             <td class="text-center"></td>
                         </tr>
                     @endforeach

8c6f092662ab947489f8cc15dfad12c2435d4ad8
tcliap <maria.aguirre@tigo.net.py>
2020-06-29 15:38:07 -0400

    agrupaciones directa

--- app/Http/Controllers/PersonaDirectaController.php
+++ app/Http/Controllers/PersonaDirectaController.php
@@ -49,7 +49,8 @@ class PersonaDirectaController extends Controller
     {
         $jefes = PersonaDirecta::where ('cargo', '=', 'representante_jefe')->get();
         $cargos_go = ['go1', 'go2', 'go3'];
-        $agrupaciones = ['MOBILE PRE', 'MOBILE POS', 'HOME', 'B2B', 'CONVERGENTE', 'LINCE', 'PUNTO FIJO'];
+        $agrupaciones = ['MOBILE PRE', 'MOBILE POS', 'HOME', 'B2B', 'CONVERGENTE', 'LINCE', 'PUNTO FIJO', 'ASESOR DELIVERY',
+                        'ASESOR DELIVERY PRE', 'ASESOR DELIVERY POS', 'ASESOR DELIVERY HOME' ];
         return view('personasDirecta.create', ['jefes'=>$jefes, 'cargos_go' => $cargos_go, 'agrupaciones'=>$agrupaciones]);
     }
 
@@ -113,8 +114,8 @@ class PersonaDirectaController extends Controller
         $persona = PersonaDirecta::findOrFail($id);
         $jefes = PersonaDirecta::where ('cargo', '=', 'representante_jefe')->get();
         $cargos_go = ['go1', 'go2', 'go3'];
-        $agrupaciones = ['MOBILE PRE', 'MOBILE POS', 'HOME', 'B2B', 'CONVERGENTE','LINCE', 'PUNTO FIJO', 'ASESOR DELIVERY'];
-
+        $agrupaciones = ['MOBILE PRE', 'MOBILE POS', 'HOME', 'B2B', 'CONVERGENTE', 'LINCE', 'PUNTO FIJO', 'ASESOR DELIVERY',
+            'ASESOR DELIVERY PRE', 'ASESOR DELIVERY POS', 'ASESOR DELIVERY HOME' ];
         return view('personasDirecta.edit', ['jefes'=>$jefes, 'persona' => $persona, 'cargos_go' => $cargos_go, 'agrupaciones'=>$agrupaciones]);
     }
 

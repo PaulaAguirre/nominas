e124c3a515b9d9d4279ae7acf7bd630e4e92fbc4
tcliap <maria.aguirre@tigo.net.py>
2020-07-20 16:31:31 -0400

    paginacion en nomina directa

--- app/Http/Controllers/NominaDirectaController.php
+++ app/Http/Controllers/NominaDirectaController.php
@@ -15,6 +15,7 @@ use Psr\Log\NullLogger;
 use App\Zona;
 
 
+
 class
 
 
@@ -59,7 +60,7 @@ NominaDirectaController extends Controller
             $personas = NominaDirecta::representanteDir($id_persona)->mes($mes)->jefesDirecta($id_jefe)
                 ->zonadirecta($id_zona, $id_jefe)
                 ->estado($estado)
-                ->orderBy('id_nomina')->get();
+                ->orderBy('id_nomina')->paginate(500);
         }
         else
         {
@@ -70,7 +71,8 @@ NominaDirectaController extends Controller
               ->whereIn('id_zona', $zonas)->get()->pluck('id_persona')->toArray();
           $personas = NominaDirecta::representanteDir($id_persona)->mes($mes)->zonadirecta($id_zona, $id_jefe)
             ->jefesDirecta($id_jefe)->estado($estado)->whereIn('id_persona_directa', $id_personas)
-            ->orderBy('id_nomina')->get();
+            ->orderBy('id_nomina')
+              ->paginate(500);
         }
 
 

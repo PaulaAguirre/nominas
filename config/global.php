a5384cfaa98758bd3586d90abc0e3dc4317e5460
tcliap <maria.aguirre@tigo.net.py>
2020-08-17 10:06:57 -0400

    habilitado seet

--- config/global.php
+++ config/global.php
@@ -4,14 +4,15 @@
  */
 
 return [
-    'mes' => 202008,
+    'mes' => 202009,
     'mes_anterior' => 202008,
 
-    'mes_tienda' => 202008,
+    'mes_tienda' => 202009,
     'mes_anterior_tienda'=>202008,
 
-    'mes_anterior_indirecta'=>202008,
     'mes_indirecta'=>202009,
+    'mes_anterior_indirecta'=>202008,
+
 ];
 
 

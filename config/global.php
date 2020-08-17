69ca9d7c8bffa0be26e2d2328e2ad02cd661ed6b
tcliap <maria.aguirre@tigo.net.py>
2020-08-17 10:00:09 -0400

    mes set

--- config/global.php
+++ config/global.php
@@ -4,14 +4,14 @@
  */
 
 return [
-    'mes' => 202008,
-    'mes_anterior' => 202007,
+    'mes' => 202009,
+    'mes_anterior' => 202009,
 
-    'mes_tienda' => 202008,
-    'mes_anterior_tienda'=>202007,
+    'mes_tienda' => 202009,
+    'mes_anterior_tienda'=>202008,
 
-    'mes_anterior_indirecta'=>202007,
-    'mes_indirecta'=>202008,
+    'mes_anterior_indirecta'=>202008,
+    'mes_indirecta'=>202009,
 ];
 
 

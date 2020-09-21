df1a40cd7d1f764dc41f28ea4dd1f69e2f91b4e7
tcliap <maria.aguirre@tigo.net.py>
2020-09-21 11:21:33 -0400

    cierre tiendas agosto

--- app/Exports/NominaTiendaRPLExport.php
+++ app/Exports/NominaTiendaRPLExport.php
@@ -13,8 +13,8 @@ class NominaTiendaRPLExport implements FromView
 
     public function view(): View
     {
-        $mes = \Config::get('global.mes_anterior_tienda');
-
+        //$mes = \Config::get('global.mes_anterior_tienda');
+        $mes = 202008;
         $asesores = NominaTiendaRPL::where('mes', '=', $mes)->get();
 
         return view('excel_tienda.exportar_tienda_mes_anterior', ['asesores'=>$asesores]);
